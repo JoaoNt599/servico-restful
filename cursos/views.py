1aa1f04c6db7a2b12bb8c23d14796a089655239f
João Vieira <netojoao599@gmail.com>
2024-06-02 14:16:42 -0300

    Criando serializers

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvalicaoSerializer
 
 # Create your views here.
+class CursoAPIView(APIView):
+
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+class AvalicaoAPIView(APIView):
+
+    def get(self, response):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvalicaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
