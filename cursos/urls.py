da3478deffbb174fdd46a6134bc36f83b797d044
João Vieira <netojoao599@gmail.com>
2024-08-05 21:12:27 -0300

    get_object e get_queryset

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,6 +4,8 @@ from .views import CursosAPIView, CursoAPIView, AvalicaoAPIView, AvalicoesAPIVie
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
     path('cursos/<int:pk>', CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', AvalicoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', AvalicaoAPIView.as_view(), name='curso_avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>', AvalicaoAPIView.as_view(), name='avaliacao'),
     path('avaliacoes/', AvalicoesAPIView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>', AvalicaoAPIView.as_view(), name='avaliacao')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvalicaoSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -11,6 +15,14 @@ class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
 class AvalicaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvalicaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(
+                self.get_queryset(), 
+                curso_id = self.kwargs.get('curso_pk'), 
+                pk = self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk = self.kwargs.get('avaliacao_pk'))
     
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
@@ -19,4 +31,10 @@ class CursosAPIView(generics.ListCreateAPIView):
 class AvalicoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvalicaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id = self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+    
    
